6f3a2267a3cd2f196c28b04ebf6acab01f67e732
Alex <ultrakiwi99@gmail.com>
2026-01-17 15:16:20 +0000

    refine husky setup

--- .husky/_/husky.sh
+++ .husky/_/husky.sh
@@ -1,2 +1,9 @@
-#!/bin/sh
-. "$(dirname "$0")/_/husky.sh"
+echo "husky - DEPRECATED
+
+Please remove the following two lines from $0:
+
+#!/usr/bin/env sh
+. \"\$(dirname -- \"\$0\")/_/husky.sh\"
+
+They WILL FAIL in v10.0.0
+"
--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -18,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, vibe-todo');
+    expect(compiled.querySelector('h1')?.textContent).toContain('My Todos');
   });
 });
